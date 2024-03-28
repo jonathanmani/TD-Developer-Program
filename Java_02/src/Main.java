ace215451ab51d7b46d58cc51d88b2a240bb74d7
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2024-03-27 20:55:02 -0400

    practiced some operations

--- Java_02/src/Main.java
+++ Java_02/src/Main.java
@@ -73,6 +73,26 @@ public class Main {
         }
 
         System.out.printf("This string is %d characters long. When reversed, it prints out as %s", strLength, reverseStr);
+
+        /*-------------------------------------------------------------------
+        04. Write a program that takes 2 strings as inputs and performs the
+        following operations:
+            a) Join 2 strings and prints it
+            b) Compare 2 strings and prints the result using the following methods
+                i) equals() method
+                ii) "==" operator
+                iii) compareTo() method
+        ------------------------------------------------------------------- */
+        Scanner scanner4 = new Scanner(System.in);
+        System.out.println("Write 2 strings (Click enter after each string): ");
+        String userStr = scanner4.nextLine();
+        String userStr2 = scanner4.nextLine();
+
+        System.out.printf("Your 2 strings were %s and %s", userStr, userStr2);
+        System.out.println("Output of equals() method: " + userStr.equals(userStr2));
+        System.out.println("Output of the == operator: " + (userStr == userStr2 ? true: false));
+        System.out.println("Output of the compareTo() method: " + userStr.compareTo(userStr2));
+
     }
     private static boolean isNumeric(String str){
         try{
